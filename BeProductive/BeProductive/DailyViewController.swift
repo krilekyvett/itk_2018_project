e743317897bce64da32d445c7a785429ac249295
Student <student@Masters-iMac-4.local>
2018-04-17 14:40:01 +0200

    update attempt4

--- BeProductive/BeProductive/DailyViewController.swift
+++ BeProductive/BeProductive/DailyViewController.swift
@@ -13,6 +13,7 @@ class DailyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // does it change?
     }
 
     override func didReceiveMemoryWarning() {
