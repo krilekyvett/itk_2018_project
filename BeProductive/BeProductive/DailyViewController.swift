a8b4c360e3f650f75d0e57283014dd839bf5d99e
krilekyvett <krilekyvett@gmail.com>
2018-05-02 10:48:53 +0200

    dailytasks v1;

--- BeProductive/BeProductive/DailyViewController.swift
+++ BeProductive/BeProductive/DailyViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DailyViewController.swift
 //  BeProductive
 //
 //  Created by Student on 2018. 04. 17..
@@ -8,10 +8,45 @@
 
 import UIKit
 
-class DailyViewController: UIViewController {
+class DailyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    
+    var list = ["Reading: War and Peace", "Buy groceries", "Finish assignment"]
+    
+        public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+        {
+            return (list.count)
+        }
+        
+        public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        {
+            let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+            cell.textLabel?.text = list[indexPath.row]
+            
+            return(cell)
+        }
+        
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+        {
+            if editingStyle == UITableViewCellEditingStyle.delete
+            {
+                list.remove(at: indexPath.row)
+                myTableView.reloadData()
+            }
+        }
+        
+        override func viewDidAppear(_ animated: Bool) {
+            myTableView.reloadData()
+        }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTableView.delegate = self
+        myTableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
         // does it change?
     }
